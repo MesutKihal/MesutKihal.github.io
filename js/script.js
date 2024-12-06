404c7290f72baeb04029fe4ed0e99a4afd19c343
messaoud <messaoudkihal@protonmail.com>
2024-12-06 22:43:06 +0100

    New Look 002

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 
+if (screen.width <= 600) {
+	const links = document.getElementsByClassName('link');
+	const icons = ["<i class='fa fa-home fa-2xl' aria-hidden='true'></i>",
+				   "<i class='fa fa-clipboard fa-2xl' aria-hidden='true'></i>",
+				   "<i class='fa fa-paper-plane fa-2xl' aria-hidden='true'></i>",
+				   "<i class='fa fa-download fa-2xl' aria-hidden='true'></i>"]
+	for (i=0;i < links.length; i++)
+	{
+		links[i].innerHTML = icons[i];
+	}
+	document.getElementById('navLinks').style.justifyContent = 'center';
+}
